b95dd42490754cfa1be74e9bfe0a61007223fe58
McCormack Leo <leo.t.mccormack@gmail.com>
2018-08-25 14:18:22 +0300

    Added 'ambiBIN' - a dedicated Ambisonic decoder for headphones with a built-in rotator;

---
+++ audio_plugins/_SPARTA_ambiBIN_/src/PluginProcessor.cpp
@@ -0,0 +1,289 @@
+/*
+ ==============================================================================
+ 
+ This file is part of SPARTA; a suite of spatial audio plug-ins.
+ Copyright (c) 2018 - Leo McCormack.
+ 
+ SPARTA is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ SPARTA is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with SPARTA.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ ==============================================================================
+*/
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+PluginProcessor::PluginProcessor()
+{
+    ringBufferInputs = new float*[MAX_NUM_CHANNELS];
+    for (int i = 0; i < MAX_NUM_CHANNELS; i++)
+        ringBufferInputs[i] = new float[FRAME_SIZE];
+    
+    ringBufferOutputs = new float*[MAX_NUM_CHANNELS];
+    for (int i = 0; i < MAX_NUM_CHANNELS; i++)
+        ringBufferOutputs[i] = new float[FRAME_SIZE];
+    
+	ambi_bin_create(&hAmbi);
+}
+
+PluginProcessor::~PluginProcessor()
+{
+	ambi_bin_destroy(&hAmbi);
+    
+    for (int i = 0; i < MAX_NUM_CHANNELS; ++i)
+        delete[] ringBufferInputs[i];
+    delete[] ringBufferInputs;
+    
+    for (int i = 0; i < MAX_NUM_CHANNELS; ++i)
+        delete[] ringBufferOutputs[i];
+    delete[] ringBufferOutputs;
+}
+
+void PluginProcessor::setParameter (int index, float newValue)
+{
+	switch (index)
+	{
+		default: break;
+	}
+}
+
+void PluginProcessor::setCurrentProgram (int index)
+{
+}
+
+float PluginProcessor::getParameter (int index)
+{
+    switch (index)
+	{
+		default: return 0.0f;
+	}
+}
+
+int PluginProcessor::getNumParameters()
+{
+	return k_NumOfParameters;
+}
+
+const String PluginProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+const String PluginProcessor::getParameterName (int index)
+{
+    switch (index)
+	{
+		default: return "NULL";
+	}
+}
+
+const String PluginProcessor::getParameterText(int index)
+{
+	return String(getParameter(index), 1);    
+}
+
+const String PluginProcessor::getInputChannelName (int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+const String PluginProcessor::getOutputChannelName (int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+double PluginProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int PluginProcessor::getNumPrograms()
+{
+    return 0;
+}
+
+int PluginProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+const String PluginProcessor::getProgramName (int index)
+{
+    return String::empty;
+}
+
+
+bool PluginProcessor::isInputChannelStereoPair (int index) const
+{
+    return true;
+}
+
+bool PluginProcessor::isOutputChannelStereoPair (int index) const
+{
+    return true;
+}
+
+
+bool PluginProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool PluginProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool PluginProcessor::silenceInProducesSilenceOut() const
+{
+    return false;
+}
+
+void PluginProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+void PluginProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+	nHostBlockSize = samplesPerBlock;
+	nSampleRate = (int)(sampleRate + 0.5);
+    
+    nNumInputs = getTotalNumInputChannels();
+    nNumOutputs = getTotalNumOutputChannels();
+ 
+	setPlayConfigDetails(nNumInputs, nNumOutputs, (double)nSampleRate, nHostBlockSize);
+	numChannelsChanged();
+    isPlaying = false;
+
+	ambi_bin_init(hAmbi, sampleRate);
+    
+    for (int i = 0; i < MAX_NUM_CHANNELS; ++i)
+        memset(ringBufferInputs[i], 0, FRAME_SIZE*sizeof(float));
+    for (int i = 0; i < MAX_NUM_CHANNELS; ++i)
+        memset(ringBufferOutputs[i], 0, FRAME_SIZE * sizeof(float));
+}
+
+void PluginProcessor::releaseResources()
+{
+    isPlaying = false;
+}
+
+void PluginProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    int nCurrentBlockSize = buffer.getNumSamples();
+    float** bufferData = buffer.getArrayOfWritePointers();
+    float** outputs = new float*[nNumOutputs];
+    for (int i = 0; i < nNumOutputs; i++)
+        outputs[i] = new float[FRAME_SIZE];
+    
+    if(nCurrentBlockSize % FRAME_SIZE == 0){ /* divisible by frame size */
+        for (int frame = 0; frame < nCurrentBlockSize/FRAME_SIZE; frame++) {
+            for (int ch = 0; ch < nNumInputs; ch++)
+                for (int i = 0; i < FRAME_SIZE; i++)
+                    ringBufferInputs[ch][i] = bufferData[ch][frame*FRAME_SIZE + i];
+            
+            /* determine if there is actually audio in the damn buffer */
+            playHead = getPlayHead();
+            bool PlayHeadAvailable = playHead->getCurrentPosition(currentPosition);
+            if (PlayHeadAvailable == true)
+                isPlaying = currentPosition.isPlaying;
+            else
+                isPlaying = true;
+            if(!isPlaying) /* for DAWs with no transport */
+                isPlaying = buffer.getRMSLevel(0, 0, nCurrentBlockSize)>1e-5f ? true : false;
+            
+            /* perform processing */
+            ambi_bin_process(hAmbi, ringBufferInputs, ringBufferOutputs, nNumInputs, nNumOutputs, FRAME_SIZE, isPlaying);
+            
+            /* replace buffer with new audio */
+            buffer.clear(frame*FRAME_SIZE, FRAME_SIZE);
+            for (int ch = 0; ch < nNumOutputs; ch++)
+                for (int i = 0; i < FRAME_SIZE; i++)
+                    bufferData[ch][frame*FRAME_SIZE + i] = ringBufferOutputs[ch][i];
+        }
+    }
+    else
+        buffer.clear();
+    
+    for (int i = 0; i < nNumOutputs; ++i)
+        delete[] outputs[i];
+    delete[] outputs;
+}
+
+//==============================================================================
+bool PluginProcessor::hasEditor() const
+{
+    return true; 
+}
+
+AudioProcessorEditor* PluginProcessor::createEditor()
+{
+    return new PluginEditor (this);
+}
+
+//==============================================================================
+void PluginProcessor::getStateInformation (MemoryBlock& destData)
+{
+    XmlElement xml("AMBIBINPLUGINSETTINGS");
+  
+    xml.setAttribute("UseDefaultHRIRset", ambi_bin_getUseDefaultHRIRsflag(hAmbi));
+    xml.setAttribute("Norm", ambi_bin_getNormType(hAmbi));
+    xml.setAttribute("ChOrder", ambi_bin_getChOrder(hAmbi));
+    xml.setAttribute("maxrE", ambi_bin_getDecEnableMaxrE(hAmbi));
+    
+    if(!ambi_bin_getUseDefaultHRIRsflag(hAmbi))
+        xml.setAttribute("SofaFilePath", String(ambi_bin_getSofaFilePath(hAmbi)));
+
+    copyXmlToBinary(xml, destData);
+}
+
+void PluginProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    ScopedPointer<XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+    int i;
+
+    if (xmlState != nullptr) {
+        if (xmlState->hasTagName("AMBIBINPLUGINSETTINGS")) {
+            
+            if(xmlState->hasAttribute("UseDefaultHRIRset"))
+                ambi_bin_setUseDefaultHRIRsflag(hAmbi, xmlState->getIntAttribute("UseDefaultHRIRset", 1));
+            if(xmlState->hasAttribute("Norm"))
+                ambi_bin_setNormType(hAmbi, xmlState->getIntAttribute("Norm", 1));
+            if(xmlState->hasAttribute("ChOrder"))
+                ambi_bin_setChOrder(hAmbi, xmlState->getIntAttribute("ChOrder", 1));
+            if(xmlState->hasAttribute("maxrE"))
+                ambi_bin_setDecEnableMaxrE(hAmbi,xmlState->getIntAttribute("maxrE", 1));
+            
+            if(xmlState->hasAttribute("SofaFilePath")){
+                String directory = xmlState->getStringAttribute("SofaFilePath", "no_file");
+                const char* new_cstring = (const char*)directory.toUTF8();
+                ambi_bin_setSofaFilePath(hAmbi, new_cstring);
+            }
+        }
+    }
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new PluginProcessor();
+}
+
