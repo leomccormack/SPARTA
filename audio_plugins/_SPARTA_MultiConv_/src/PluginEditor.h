1ff34b34819b173a80008d883eb924ada4b1a05e
McCormack Leo <leo.t.mccormack@gmail.com>
2019-09-25 14:16:46 +0300

    added MultiConv, a multi-channel convolver;

---
+++ audio_plugins/_SPARTA_MultiConv_/src/PluginEditor.h
@@ -0,0 +1,87 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.4
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+//[Headers]     -- You can add your own extra header files here --
+
+#include "JuceHeader.h"
+#include "PluginProcessor.h"
+
+typedef enum _SPARTA_WARNINGS{
+    k_warning_none,
+    k_warning_frameSize,
+    k_warning_NinputCH,
+    k_warning_NoutputCH,
+    k_warning_osc_connection_fail
+}SPARTA_WARNINGS;
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Introjucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class PluginEditor  : public AudioProcessorEditor,
+                      public Timer
+{
+public:
+    //==============================================================================
+    PluginEditor (PluginProcessor* ownerFilter);
+    ~PluginEditor();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+
+    //[/UserMethods]
+
+    void paint (Graphics& g) override;
+    void resized() override;
+
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    PluginProcessor* hVst;
+    void* hMC;
+    void timerCallback() override;
+
+    /* warnings */
+    SPARTA_WARNINGS currentWarning;
+
+    /* tooltips */
+    SharedResourcePointer<TooltipWindow> tipWindow;
+
+    //[/UserVariables]
+
+    //==============================================================================
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginEditor)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
