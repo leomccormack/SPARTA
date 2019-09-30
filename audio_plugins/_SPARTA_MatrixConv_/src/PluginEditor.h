ec699c7709a2912ae4eacd03c1c586f610a82e6f
McCormack Leo <leo.t.mccormack@gmail.com>
2019-09-30 17:07:39 +0300

    Added MatrixConv, a matrix convolver;

---
+++ audio_plugins/_SPARTA_MatrixConv_/src/PluginEditor.h
@@ -0,0 +1,131 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.4
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+//[Headers]     -- You can add your own extra header files here --
+
+#include "JuceHeader.h"
+#include "PluginProcessor.h"
+
+typedef enum _SPARTA_WARNINGS{
+    k_warning_none,
+    k_warning_sampleRate_missmatch,
+    k_warning_nInputs_more_than_64,
+    k_warning_nOutputs_more_than_64
+
+}SPARTA_WARNINGS;
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Introjucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class PluginEditor  : public AudioProcessorEditor,
+                      public Timer,
+                      private FilenameComponentListener,
+                      public Button::Listener,
+                      public Slider::Listener
+{
+public:
+    //==============================================================================
+    PluginEditor (PluginProcessor* ownerFilter);
+    ~PluginEditor();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+
+    //[/UserMethods]
+
+    void paint (Graphics& g) override;
+    void resized() override;
+    void buttonClicked (Button* buttonThatWasClicked) override;
+    void sliderValueChanged (Slider* sliderThatWasMoved) override;
+
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    PluginProcessor* hVst;
+    void* hMC;
+    void timerCallback() override;
+
+    /* sofa loading */
+    FilenameComponent fileChooser;
+
+    /* warnings */
+    SPARTA_WARNINGS currentWarning;
+
+    /* wav file loading */
+    AudioFormatManager formatManager;
+    AudioSampleBuffer fileBuffer;
+    float durationInSeconds;
+    void filenameComponentChanged (FilenameComponent*) override  {
+        String directory = fileChooser.getCurrentFile().getFullPathName();
+        hVst->setWavDirectory(directory);
+        loadWavFile();
+    }
+    void loadWavFile()
+    {
+        String directory = hVst->getWavDirectory();
+        std::unique_ptr<AudioFormatReader> reader (formatManager.createReaderFor (directory));
+
+        if (reader.get() != nullptr) { /* if file exists */
+            durationInSeconds = (float)reader->lengthInSamples / (float)reader->sampleRate;
+
+            if (reader->numChannels <= 1024 /* maximum number of channels for WAV files */) {
+                fileBuffer.setSize (reader->numChannels, (int) reader->lengthInSamples);
+                reader->read (&fileBuffer, 0, (int) reader->lengthInSamples, 0, true, true);
+            }
+            const float** H = fileBuffer.getArrayOfReadPointers();
+            matrixconv_setFilters(hMC, H, fileBuffer.getNumChannels(), fileBuffer.getNumSamples(), reader->sampleRate);
+        }
+    }
+
+    /* tooltips */
+    SharedResourcePointer<TooltipWindow> tipWindow;
+
+    //[/UserVariables]
+
+    //==============================================================================
+    std::unique_ptr<ToggleButton> TBenablePartConv;
+    std::unique_ptr<Label> label_hostBlockSize;
+    std::unique_ptr<Label> label_NFilters;
+    std::unique_ptr<Label> label_filterLength;
+    std::unique_ptr<Label> label_hostfs;
+    std::unique_ptr<Label> label_filterfs;
+    std::unique_ptr<Slider> SL_num_inputs;
+    std::unique_ptr<Label> label_MatrixNInputs;
+    std::unique_ptr<Label> label_MatrixNoutputs;
+    std::unique_ptr<Label> label_NOutputs;
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginEditor)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
---
+++ audio_plugins/_SPARTA_MatrixConv_/src/PluginProcessor.cpp
@@ -0,0 +1,227 @@
+/*
+ ==============================================================================
+ 
+ This file is part of SPARTA; a suite of spatial audio plug-ins.
+ Copyright (c) 2018 - Leo McCormack.
+ 
+ SPARTA is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ SPARTA is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with SPARTA.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ ==============================================================================
+*/
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+PluginProcessor::PluginProcessor() : 
+	AudioProcessor(BusesProperties()
+		.withInput("Input", AudioChannelSet::discreteChannels(64), true)
+	    .withOutput("Output", AudioChannelSet::discreteChannels(64), true))
+{
+	nSampleRate = 48000;
+    nHostBlockSize = -1;
+    lastWavDirectory = TRANS("no_file");
+	matrixconv_create(&hMCnv);
+}
+
+PluginProcessor::~PluginProcessor()
+{
+	matrixconv_destroy(&hMCnv);
+}
+
+void PluginProcessor::setParameter (int index, float newValue)
+{
+	switch (index) {
+		default: break;
+	}
+}
+
+void PluginProcessor::setCurrentProgram (int index)
+{
+}
+
+float PluginProcessor::getParameter (int index)
+{
+    switch (index) {
+		default: return 0.0f;
+	}
+}
+
+int PluginProcessor::getNumParameters()
+{
+	return k_NumOfParameters;
+}
+
+const String PluginProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+const String PluginProcessor::getParameterName (int index)
+{
+    switch (index){
+		default: return "NULL";
+	}
+}
+
+const String PluginProcessor::getParameterText(int index)
+{
+    switch (index) {
+        default: return "NULL";
+    }
+}
+
+const String PluginProcessor::getInputChannelName (int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+const String PluginProcessor::getOutputChannelName (int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+double PluginProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int PluginProcessor::getNumPrograms()
+{
+    return 1;
+}
+
+int PluginProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+const String PluginProcessor::getProgramName (int index)
+{
+    return String();
+}
+
+
+bool PluginProcessor::isInputChannelStereoPair (int index) const
+{
+    return true;
+}
+
+bool PluginProcessor::isOutputChannelStereoPair (int index) const
+{
+    return true;
+}
+
+
+bool PluginProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool PluginProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool PluginProcessor::silenceInProducesSilenceOut() const
+{
+    return false;
+}
+
+void PluginProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+void PluginProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    nHostBlockSize = samplesPerBlock;
+    nNumInputs =  getTotalNumInputChannels();
+    nNumOutputs = getTotalNumOutputChannels();
+    nSampleRate = (int)(sampleRate + 0.5);
+    isPlaying = false;
+
+	matrixconv_init(hMCnv, (float)sampleRate, nHostBlockSize);
+}
+
+void PluginProcessor::releaseResources()
+{
+}
+
+void PluginProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    int nCurrentBlockSize = nHostBlockSize = buffer.getNumSamples();
+    nNumInputs = jmin(getTotalNumInputChannels(), buffer.getNumChannels());
+    nNumOutputs = jmin(getTotalNumOutputChannels(), buffer.getNumChannels());
+    float** bufferData = buffer.getArrayOfWritePointers();
+
+    if(nCurrentBlockSize == nHostBlockSize)
+        matrixconv_process(hMCnv, bufferData, bufferData, nNumInputs, nNumOutputs, nCurrentBlockSize);
+    else
+        buffer.clear();
+}
+
+//==============================================================================
+bool PluginProcessor::hasEditor() const
+{
+    return true; 
+}
+
+AudioProcessorEditor* PluginProcessor::createEditor()
+{
+    return new PluginEditor (this);
+}
+
+//==============================================================================
+void PluginProcessor::getStateInformation (MemoryBlock& destData)
+{
+	/* Create an outer XML element.. */ 
+	XmlElement xml("MATRIXCONVAUDIOPLUGINSETTINGS");
+    xml.setAttribute("LastWavFilePath", lastWavDirectory);
+    xml.setAttribute("usePartitionedConv", matrixconv_getEnablePart(hMCnv));
+    xml.setAttribute("numInputChannels", matrixconv_getNumInputChannels(hMCnv));
+	copyXmlToBinary(xml, destData);
+}
+
+void PluginProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+	/* This getXmlFromBinary() function retrieves XML from the binary blob */
+    std::unique_ptr<XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+
+	if (xmlState != nullptr) {
+		/* make sure that it's actually the correct XML object */
+		if (xmlState->hasTagName("MATRIXCONVAUDIOPLUGINSETTINGS")) {
+ 
+            if(xmlState->hasAttribute("LastWavFilePath"))
+                lastWavDirectory = xmlState->getStringAttribute("LastWavFilePath", "no_file");
+            if(xmlState->hasAttribute("usePartitionedConv"))
+                matrixconv_setEnablePart(hMCnv, xmlState->getIntAttribute("usePartitionedConv", 1));
+            if(xmlState->hasAttribute("numInputChannels"))
+                matrixconv_setNumInputChannels(hMCnv, xmlState->getIntAttribute("numInputChannels", 1));
+        }
+	}
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new PluginProcessor();
+}
+
