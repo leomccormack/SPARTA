6295119a7965fcecd356527e2328ed91a94bde0c
leomccormack <leo.t.mccormack@gmail.com>
2021-03-11 18:19:16 +0200

    added mouse support to panner view for roomsim GUI

--- audio_plugins/_SPARTA_ambiRoomSim/src/pannerView.cpp
+++ audio_plugins/_SPARTA_ambiRoomSim/src/pannerView.cpp
@@ -27,8 +27,6 @@
 //[MiscUserDefs] You can add your own user definitions and misc code here...
 const float iconWidth = 8.0f;
 const float iconRadius = iconWidth/2.0f;
-const float circleWidth = 168.0f;
-const float circleRadius = circleWidth / 2.0f;
 //[/MiscUserDefs]
 
 //==============================================================================
@@ -49,6 +47,7 @@ pannerView::pannerView (PluginProcessor* ownerFilter, int _width, int _height)
     hAmbi = hVst->getFXHandle();
     width = _width;
     height = _height;
+    topOrSideView = TOP_VIEW; /* default */
 
     for(int src=0; src<MAX_NUM_CHANNELS; src++){
 //        SourceIcons[src].setBounds(width - width*(ambi_roomsim_getSourceAzi_deg(hAmbi, src) + 180.0f)/360.f - icon_size/2.0f,
@@ -168,7 +167,7 @@ void pannerView::paint (juce::Graphics& g)
     g.setColour(Colours::orange);
     g.setOpacity(0.5f);
     for(int src=0; src<ambi_roomsim_getNumSources(hAmbi); src++){
-        float point_x = view_x + room_dims_pixels[1] - scale*(ambi_roomsim_getSourceY(hAmbi, src)); /* subtract by half the room dimensions to align to centre */
+        float point_x = view_x + room_dims_pixels[1] - scale*(ambi_roomsim_getSourceY(hAmbi, src));
         float point_y = view_y + room_dims_pixels[2] - scale*(ambi_roomsim_getSourceZ(hAmbi, src));
         srcIcon.setBounds(point_x-iconRadius, point_y-iconRadius, iconWidth, iconWidth);
         g.fillEllipse(srcIcon);
@@ -178,7 +177,7 @@ void pannerView::paint (juce::Graphics& g)
     g.setColour(Colours::magenta);
     g.setOpacity(0.5f);
     for(int rec=0; rec<ambi_roomsim_getNumReceivers(hAmbi); rec++){
-        float point_x = view_x + room_dims_pixels[1] - scale*(ambi_roomsim_getReceiverY(hAmbi, rec)); /* subtract by half the room dimensions to align to centre */
+        float point_x = view_x + room_dims_pixels[1] - scale*(ambi_roomsim_getReceiverY(hAmbi, rec));
         float point_y = view_y + room_dims_pixels[2] - scale*(ambi_roomsim_getReceiverZ(hAmbi, rec));
         recIcon.setBounds(point_x-iconRadius, point_y-iconRadius, iconWidth, iconWidth);
         g.fillEllipse(recIcon);
@@ -199,71 +198,128 @@ void pannerView::mouseDown (const juce::MouseEvent& e)
 {
     //[UserCode_mouseDown] -- Add your code here...
 
+    Rectangle<int> srcIcon, recIcon;
+    const float room_pixels = 220;
+    float room_dims_pixels[3], room_dims_m[3];
+    room_dims_m[0] = ambi_roomsim_getRoomDimX(hAmbi);
+    room_dims_m[1] = ambi_roomsim_getRoomDimY(hAmbi);
+    room_dims_m[2] = ambi_roomsim_getRoomDimZ(hAmbi);
+
+    /* Scaling factor to convert metres to pixels */
+    float scale = room_pixels/MAX(MAX(room_dims_m[0], room_dims_m[1]), room_dims_m[2]);
+    room_dims_pixels[0] = room_dims_m[0]*scale;
+    room_dims_pixels[1] = room_dims_m[1]*scale;
+    room_dims_pixels[2] = room_dims_m[2]*scale;
+
     /* TOP VIEW */
+    float view_x = 22.0f; float view_y = 22.0f;
+    for(int src=0; src<ambi_roomsim_getNumSources(hAmbi); src++){
+        float point_x = view_x + room_dims_pixels[1] - scale*(ambi_roomsim_getSourceY(hAmbi, src));
+        float point_y = view_y + room_dims_pixels[0] - scale*(ambi_roomsim_getSourceX(hAmbi, src));
+        srcIcon.setBounds(point_x-iconRadius, point_y-iconRadius, iconWidth, iconWidth);
+        if(srcIcon.expanded(4, 4).contains(e.getMouseDownPosition())){
+            sourceIconIsClicked = true;
+            indexOfClickedIcon = src;
+            topOrSideView = TOP_VIEW;
+            return;
+        }
+    }
+    for(int rec=0; rec<ambi_roomsim_getNumReceivers(hAmbi); rec++){
+        float point_x = view_x + room_dims_pixels[1] - scale*(ambi_roomsim_getReceiverY(hAmbi, rec));
+        float point_y = view_y + room_dims_pixels[0] - scale*(ambi_roomsim_getReceiverX(hAmbi, rec));
+        recIcon.setBounds(point_x-iconRadius, point_y-iconRadius, iconWidth, iconWidth);
+        if(recIcon.expanded(4, 4).contains(e.getMouseDownPosition())){
+            receiverIconIsClicked = true;
+            indexOfClickedIcon = rec;
+            topOrSideView = TOP_VIEW;
+            return;
+        }
+    }
 
-
-
-//    float view_x = 22.0f , view_y = 72.0f;
-//    float centre_x = view_x+circleRadius;
-//    float centre_y = view_y+circleRadius;
-//    float listener_x = -circleRadius * hcompass_getListenerY(hCmp, listID)/hcompass_getSourceDistance(hCmp);
-//    float listener_y = -circleRadius * hcompass_getListenerX(hCmp, listID)/hcompass_getSourceDistance(hCmp);
-//    Rectangle<int> icon_int;
-//    icon_int.setBounds(centre_x-iconRadius+listener_x,
-//                       centre_y-iconRadius+listener_y,
-//                       iconWidth, iconWidth);
-//    if(icon_int.expanded(4, 4).contains(e.getMouseDownPosition())){
-//        iconIsClicked = true;
-//        indexOfClickedIcon = 0; /* top */
-//        return;
-//    }
-//
-//    /* SIDE VIEW */
-//    view_x = 210.0f;
-//    view_y = 72.0f;
-//    centre_x = view_x+circleRadius;
-//    centre_y = view_y+circleRadius;
-//    listener_x = -circleRadius * hcompass_getListenerY(hCmp, listID)/hcompass_getSourceDistance(hCmp);
-//    listener_y = -circleRadius * hcompass_getListenerZ(hCmp, listID)/hcompass_getSourceDistance(hCmp);
-//    icon_int.setBounds(centre_x-iconRadius+listener_x,
-//                       centre_y-iconRadius+listener_y,
-//                       iconWidth, iconWidth);
-//    if(icon_int.expanded(4, 4).contains(e.getMouseDownPosition())){
-//        iconIsClicked = true;
-//        indexOfClickedIcon = 1; /* side */
-//        return;
-//    }
-
+    /* SIDE VIEW */
+    view_x = 22.0f; view_y = 262.0f;
+    for(int src=0; src<ambi_roomsim_getNumSources(hAmbi); src++){
+        float point_x = view_x + room_dims_pixels[1] - scale*(ambi_roomsim_getSourceY(hAmbi, src));
+        float point_y = view_y + room_dims_pixels[2] - scale*(ambi_roomsim_getSourceZ(hAmbi, src));
+        srcIcon.setBounds(point_x-iconRadius, point_y-iconRadius, iconWidth, iconWidth);
+        if(srcIcon.expanded(4, 4).contains(e.getMouseDownPosition())){
+            sourceIconIsClicked = true;
+            indexOfClickedIcon = src;
+            topOrSideView = SIDE_VIEW;
+            return;
+        }
+    }
+    for(int rec=0; rec<ambi_roomsim_getNumReceivers(hAmbi); rec++){
+        float point_x = view_x + room_dims_pixels[1] - scale*(ambi_roomsim_getReceiverY(hAmbi, rec));
+        float point_y = view_y + room_dims_pixels[2] - scale*(ambi_roomsim_getReceiverZ(hAmbi, rec));
+        recIcon.setBounds(point_x-iconRadius, point_y-iconRadius, iconWidth, iconWidth);
+        if(recIcon.expanded(4, 4).contains(e.getMouseDownPosition())){
+            receiverIconIsClicked = true;
+            indexOfClickedIcon = rec;
+            topOrSideView = SIDE_VIEW;
+            return;
+        }
+    }
     //[/UserCode_mouseDown]
 }
 
 void pannerView::mouseDrag (const juce::MouseEvent& e)
 {
     //[UserCode_mouseDrag] -- Add your code here...
-//    if(iconIsClicked){
-//        Point<float> point;
-//        float view_x, view_y, centre_x, centre_y;
-//        switch(indexOfClickedIcon){
-//            case 0: /* TOP VIEW */
-//                view_x = 22.0f; view_y = 72.0f;
-//                centre_x = view_x+circleRadius;
-//                centre_y = view_y+circleRadius;
-//                point.setXY((float)e.getPosition().getX()-2, (float)e.getPosition().getY()-2);
-//                hcompass_setListenerY(hCmp, listID, ((centre_x-point.getX()) * hcompass_getSourceDistance(hCmp)/circleRadius));
-//                hcompass_setListenerX(hCmp, listID, ((centre_y-point.getY()) * hcompass_getSourceDistance(hCmp)/circleRadius));
-//                break;
-//
-//            case 1: /* SIDE VIEW */
-//                view_x = 210.0f; view_y = 72.0f;
-//                centre_x = view_x+circleRadius;
-//                centre_y = view_y+circleRadius;
-//                point.setXY((float)e.getPosition().getX()-2, (float)e.getPosition().getY()-2);
-//                hcompass_setListenerY(hCmp, listID, ((centre_x-point.getX()) * hcompass_getSourceDistance(hCmp)/circleRadius));
-//                hcompass_setListenerZ(hCmp, listID, ((centre_y-point.getY()) * hcompass_getSourceDistance(hCmp)/circleRadius));
-//                break;
-//
-//        }
-//    }
+
+    const float room_pixels = 220;
+    float room_dims_pixels[3], room_dims_m[3];
+    float scale;
+    Point<float> point;
+    float view_x, view_y;
+
+    if(sourceIconIsClicked||receiverIconIsClicked){
+        /* Scaling factor to convert metres to pixels */
+        room_dims_m[0] = ambi_roomsim_getRoomDimX(hAmbi);
+        room_dims_m[1] = ambi_roomsim_getRoomDimY(hAmbi);
+        room_dims_m[2] = ambi_roomsim_getRoomDimZ(hAmbi);
+        scale = room_pixels/MAX(MAX(room_dims_m[0], room_dims_m[1]), room_dims_m[2]);
+        room_dims_pixels[0] = room_dims_m[0]*scale;
+        room_dims_pixels[1] = room_dims_m[1]*scale;
+        room_dims_pixels[2] = room_dims_m[2]*scale;
+    }
+
+    if(sourceIconIsClicked){
+        switch(topOrSideView){
+            case TOP_VIEW:
+                view_x = 22.0f; view_y = 22.0f;
+                point.setXY((float)e.getPosition().getX()-2, (float)e.getPosition().getY()-2);
+                ambi_roomsim_setSourceY(hAmbi, indexOfClickedIcon, -(point.getX() - view_x - room_dims_pixels[1])/scale);
+                ambi_roomsim_setSourceX(hAmbi, indexOfClickedIcon, -(point.getY() - view_y - room_dims_pixels[0])/scale);
+                break;
+
+            case SIDE_VIEW:
+                view_x = 22.0f; view_y = 262.0f;
+                point.setXY((float)e.getPosition().getX()-2, (float)e.getPosition().getY()-2);
+                ambi_roomsim_setSourceY(hAmbi, indexOfClickedIcon, -(point.getX() - view_x - room_dims_pixels[1])/scale);
+                ambi_roomsim_setSourceZ(hAmbi, indexOfClickedIcon, -(point.getY() - view_y - room_dims_pixels[2])/scale);
+                break;
+            default: assert(0); break;
+        }
+    }
+    else if(receiverIconIsClicked){
+        switch(topOrSideView){
+            case TOP_VIEW:
+                view_x = 22.0f; view_y = 22.0f;
+                point.setXY((float)e.getPosition().getX()-2, (float)e.getPosition().getY()-2);
+                ambi_roomsim_setReceiverY(hAmbi, indexOfClickedIcon, -(point.getX() - view_x - room_dims_pixels[1])/scale);
+                ambi_roomsim_setReceiverX(hAmbi, indexOfClickedIcon, -(point.getY() - view_y - room_dims_pixels[0])/scale);
+                break;
+
+            case SIDE_VIEW:
+                view_x = 22.0f; view_y = 262.0f;
+                point.setXY((float)e.getPosition().getX()-2, (float)e.getPosition().getY()-2);
+                ambi_roomsim_setReceiverY(hAmbi, indexOfClickedIcon, -(point.getX() - view_x - room_dims_pixels[1])/scale);
+                ambi_roomsim_setReceiverZ(hAmbi, indexOfClickedIcon, -(point.getY() - view_y - room_dims_pixels[2])/scale);
+                break;
+            default: assert(0); break;
+        }
+    }
     //[/UserCode_mouseDrag]
 }
 
