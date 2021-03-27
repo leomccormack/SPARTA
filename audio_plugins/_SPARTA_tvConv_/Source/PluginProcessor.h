2fb57b9d07ed54ec88d3049cdd763b8d17884d00
Rapoolas Daugintis <rapolas.daugintis@aalto.fi>
2021-03-27 15:15:45 +0200

    matrixvonv plugin adapted to tvconv

--- audio_plugins/_SPARTA_tvConv_/Source/PluginProcessor.h
+++ audio_plugins/_SPARTA_tvConv_/Source/PluginProcessor.h
@@ -10,12 +10,58 @@
 
 #include <JuceHeader.h>
 #include "tvconv.h"
+#include <string.h>
+#define BUILD_VER_SUFFIX "alpha" /* String to be added before the version name on the GUI (beta, alpha etc..) */
+#ifndef MIN
+# define MIN(a,b) (( (a) < (b) ) ? (a) : (b))
+#endif
+#ifndef MAX
+# define MAX(a,b) (( (a) > (b) ) ? (a) : (b))
+#endif
 
+enum {
+    /* For the default VST GUI */
+    k_NumOfParameters
+};
 //==============================================================================
 /**
 */
-class PluginProcessor  : public juce::AudioProcessor
+class PluginProcessor  : public AudioProcessor,
+                         public VSTCallbackHandler
 {
+public:
+    /* Set/Get functions */
+        void* getFXHandle() { return hTVCnv; }
+        int getCurrentBlockSize(){ return nHostBlockSize; }
+        int getCurrentNumInputs(){ return nNumInputs; }
+        int getCurrentNumOutputs(){ return nNumOutputs; }
+        void setSofaDirectory(String newDirectory){
+            lastSofaDirectory = newDirectory;
+        }
+        String getSofaDirectory(){ return lastSofaDirectory; }
+    
+    /* VST CanDo */
+    pointer_sized_int handleVstManufacturerSpecific (int32 /*index*/, pointer_sized_int /*value*/, void* /*ptr*/, float /*opt*/) override { return 0; }
+    pointer_sized_int handleVstPluginCanDo (int32 /*index*/, pointer_sized_int /*value*/, void* ptr, float /*opt*/) override{
+        auto text = (const char*) ptr;
+        auto matches = [=](const char* s) { return strcmp (text, s) == 0; };
+        if (matches ("wantsChannelCountNotifications"))
+            return 1;
+        return 0;
+    }
+    
+private:
+    void* hTVCnv;         /* tvconv handle */
+    int nNumInputs;       /* current number of input channels */
+    int nNumOutputs;      /* current number of output channels */
+    int nSampleRate;      /* current host sample rate */
+    int nHostBlockSize;   /* typical host block size to expect, in samples */
+    String lastSofaDirectory;
+    
+    
+/***************************************************************************\
+                    JUCE Functions
+\***************************************************************************/
 public:
     //==============================================================================
     PluginProcessor();
@@ -55,12 +101,6 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
-    void* hTVCnv;            /* tvconv handle */
-    int nNumInputs;       /* current number of input channels */
-    int nNumOutputs;      /* current number of output channels */
-    int nSampleRate;      /* current host sample rate */
-    int nHostBlockSize;   /* typical host block size to expect, in samples */
-    File lastDir;
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginProcessor)
 };
