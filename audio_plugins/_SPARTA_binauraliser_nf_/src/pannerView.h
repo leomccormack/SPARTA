558213b091d6f28f782576fff342b03f3b788667
Michael McCrea <mtmccrea@users.noreply.github.com>
2022-03-03 12:43:36 +0200

    BinauraliserNF: DRY refactore and UI cleanup
    DRY refactor with updateSrcLsIconBounds.
    
    HRIR icons are now circles, smaller.
    
    UI Cleanup.
    
    Comment cleanup

--- audio_plugins/_SPARTA_binauraliser_nf_/src/pannerView.h
+++ audio_plugins/_SPARTA_binauraliser_nf_/src/pannerView.h
@@ -48,6 +48,7 @@ public:
     //[UserMethods]     -- You can add your own custom methods in this section.
 
     void refreshPanView();
+    void updateSrcLsIconBounds();
     void setShowInputs(bool state){ showInputs = state; }
     void setShowOutputs(bool state){ showOutputs = state; }
     bool getSourceIconIsClicked(){ return sourceIconIsClicked; }
@@ -79,8 +80,10 @@ private:
     bool sourceIconIsClicked;
     int indexOfClickedSource;
     bool soloActive;
-    float icon_diam;
-    float icon_radius;
+    float icon_diam_src;
+    float icon_radius_src;
+    float icon_diam_hrir;
+    float icon_radius_hrir;
     NormalisableRange<float> distRange;
     NormalisableRange<float> iconGrowFac;
     float ffThresh;
